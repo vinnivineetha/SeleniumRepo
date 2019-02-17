2dd3525561e55a97e8494b7c2cd2fa62cb2d17d2
vinnivineetha <vineethag207@gmail.com>
2019-02-17 13:33:39 +0530

    my second commit

--- src/main/java/org/sspart/mySeleniumClass/App.java
+++ src/main/java/org/sspart/mySeleniumClass/App.java
@@ -1,6 +1,8 @@
 package org.sspart.mySeleniumClass;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
@@ -9,12 +11,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) 
     {
         System.out.println( "Hello World!" );
         System.setProperty("webdriver.chrome.driver","C:/Users/vinni/Downloads/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
         driver.get("https://www.amazon.in/");
+        
+	   driver.findElement(By.linkText("Customer Service")).click();
+	   driver.findElement(By.xpath("//a[contains(text(),'Your Orders')]")).click();
+	   driver.findElement(By.xpath("//input[@name='email']")).sendKeys("vineethag207@gmail.com");
+	   driver.findElement(By.xpath("//input[@id='continue']")).click();
+	   driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/form[1]/div[1]/div[1]/div[1]/div[2]/input[1]")).sendKeys("august20");
+	   driver.findElement(By.xpath("//input[@id='signInSubmit']")).click();
+	   try {
+		Thread.sleep(5000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	   
         driver.quit();
     }
 }
